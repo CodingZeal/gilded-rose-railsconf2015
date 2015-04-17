a940b70d3c9f33b2f185aa6955010b44d44ac314
Randy Coulman <rcoulman@gmail.com>
2015-04-16 18:33:04 -0700

    Add characterization tests

--- test/gilded_rose_test.rb
+++ test/gilded_rose_test.rb
@@ -1,11 +1,21 @@
 require_relative 'helper'
+require 'approvals'
 
 class GildedRoseTest < Minitest::Test
   def setup
     @gilded_rose = GildedRose.new
   end
 
-  def test_does_something
-    @gilded_rose.update_quality
+  20.times do |i|
+    define_method("test_after_#{i}_updates") do
+      i.times { @gilded_rose.update_quality }
+      Approvals.verify(items, name: "after #{i} updates")
+    end
+  end
+
+  def items
+    @gilded_rose.instance_variable_get(:@items).map do |item|
+      "Name: #{item.name} Sell in: #{item.sell_in} Quality: #{item.quality}"
+    end
   end
 end
