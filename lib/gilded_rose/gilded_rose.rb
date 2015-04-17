cab5778231e18635fa908edfae49e97fda904852
Randy Coulman <rcoulman@gmail.com>
2015-04-16 18:39:40 -0700

    Use += and -=

--- lib/gilded_rose/gilded_rose.rb
+++ lib/gilded_rose/gilded_rose.rb
@@ -1,9 +1,6 @@
 require_relative 'item'
 
 class GildedRose
-
-  @items = []
-
   def initialize
     @items = []
     @items << Item.new("+5 Dexterity Vest", 10, 20)
@@ -20,43 +17,43 @@ class GildedRose
       if (@items[i].name != "Aged Brie" && @items[i].name != "Backstage passes to a TAFKAL80ETC concert")
         if (@items[i].quality > 0)
           if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-            @items[i].quality = @items[i].quality - 1
+            @items[i].quality -= 1
           end
         end
       else
         if (@items[i].quality < 50)
-          @items[i].quality = @items[i].quality + 1
+          @items[i].quality += 1
           if (@items[i].name == "Backstage passes to a TAFKAL80ETC concert")
             if (@items[i].sell_in < 11)
               if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
+                @items[i].quality += 1
               end
             end
             if (@items[i].sell_in < 6)
               if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
+                @items[i].quality += 1
               end
             end
           end
         end
       end
       if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-        @items[i].sell_in = @items[i].sell_in - 1;
+        @items[i].sell_in -= 1;
       end
       if (@items[i].sell_in < 0)
         if (@items[i].name != "Aged Brie")
           if (@items[i].name != "Backstage passes to a TAFKAL80ETC concert")
             if (@items[i].quality > 0)
               if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-                @items[i].quality = @items[i].quality - 1
+                @items[i].quality -= 1
               end
             end
           else
-            @items[i].quality = @items[i].quality - @items[i].quality
+            @items[i].quality = 0
           end
         else
           if (@items[i].quality < 50)
-            @items[i].quality = @items[i].quality + 1
+            @items[i].quality += 1
           end
         end
       end
