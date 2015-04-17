42d4cac73c2d002c35adf85f1fefbf7f4019cb85
Randy Coulman <rcoulman@gmail.com>
2015-04-16 19:11:29 -0700

    Extract more duplication

--- lib/gilded_rose/gilded_rose.rb
+++ lib/gilded_rose/gilded_rose.rb
@@ -12,52 +12,58 @@ class GildedRose
   end
 
   def update_quality
-
     @items.each do |item|
-      if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality -= 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality += 1
-              end
-            end
-          end
-        end
-      end
+      update_item(item)
+    end
+  end
+
+  def update_item(item)
+    if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
       if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in -= 1
+        decrease_quality(item)
       end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality -= 1
-              end
-            end
-          else
-            item.quality = 0
+    else
+      if item.quality < 50
+        item.quality += 1
+        if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          increase_quality_if_less_than(item, 11)
+          increase_quality_if_less_than(item, 6)
+        end
+      end
+    end
+    if item.name != "Sulfuras, Hand of Ragnaros"
+      item.sell_in -= 1
+    end
+    if item.sell_in < 0
+      if item.name != "Aged Brie"
+        if item.name != "Backstage passes to a TAFKAL80ETC concert"
+          if item.name != "Sulfuras, Hand of Ragnaros"
+            decrease_quality(item)
           end
         else
-          if item.quality < 50
-            item.quality += 1
-          end
+          item.quality = 0
         end
+      else
+        increase_quality(item)
       end
     end
   end
 
+  def decrease_quality(item)
+    if item.quality > 0
+      item.quality -= 1
+    end
+  end
+
+  def increase_quality_if_less_than(item, days)
+    if item.sell_in < days
+      increase_quality(item)
+    end
+  end
+
+  def increase_quality(item)
+    if item.quality < 50
+      item.quality += 1
+    end
+  end
 end
--- test/gilded_rose_test.rb
+++ test/gilded_rose_test.rb
@@ -13,6 +13,20 @@ class GildedRoseTest < Minitest::Test
     end
   end
 
+  def test_normal_item
+    item = Item.new("+5 Dexterity Vest", 10, 20)
+    @gilded_rose.update_item(item)
+    assert_equal 9, item.sell_in, "sell in"
+    assert_equal 19, item.quality, "quality"
+  end
+
+  def test_normal_item_past_sell_in
+    item = Item.new("+5 Dexterity Vest", 0, 20)
+    @gilded_rose.update_item(item)
+    assert_equal -1, item.sell_in, "sell in"
+    assert_equal 18, item.quality, "quality"
+  end
+
   def items
     @gilded_rose.instance_variable_get(:@items).map do |item|
       "Name: #{item.name} Sell in: #{item.sell_in} Quality: #{item.quality}"
