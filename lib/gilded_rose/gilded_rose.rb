d1bc09498bb7d7f8071bc2dede77fa2a590ef50f
Randy Coulman <rcoulman@gmail.com>
2015-04-22 15:33:53 -0400

    Refactor and add new feature

--- lib/gilded_rose/gilded_rose.rb
+++ lib/gilded_rose/gilded_rose.rb
@@ -1,65 +1,78 @@
 require_relative 'item'
 
 class GildedRose
+  attr_reader :items
 
-  @items = []
+  MAX_QUALITY = 50
 
   def initialize
     @items = []
-    @items << Item.new("+5 Dexterity Vest", 10, 20)
-    @items << Item.new("Aged Brie", 2, 0)
-    @items << Item.new("Elixir of the Mongoose", 5, 7)
-    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
-    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
-    @items << Item.new("Conjured Mana Cake", 3, 6)
+    items << Item.new("+5 Dexterity Vest", 10, 20)
+    items << Item.new("Aged Brie", 2, 0)
+    items << Item.new("Elixir of the Mongoose", 5, 7)
+    items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
+    items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    items << Item.new("Conjured Mana Cake", 3, 6)
   end
 
   def update_quality
+    items.each { |item| update_item(item) }
+  end
 
-    for i in 0..(@items.size-1)
-      if (@items[i].name != "Aged Brie" && @items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-        if (@items[i].quality > 0)
-          if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-            @items[i].quality = @items[i].quality - 1
+  def update_item(item)
+    if item.name =~ /Conjured/
+      2.times { decrease_item_quality(item) }
+      item.sell_in -= 1
+      if (item.sell_in < 0)
+        2.times { decrease_item_quality(item) }
+      end
+      return
+    end
+
+    if item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if item.name != "Sulfuras, Hand of Ragnaros"
+        decrease_item_quality(item)
+      end
+    else
+      if item.quality < MAX_QUALITY
+        item.quality += 1
+        if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if item.sell_in < 11
+            increase_item_quality(item)
           end
-        end
-      else
-        if (@items[i].quality < 50)
-          @items[i].quality = @items[i].quality + 1
-          if (@items[i].name == "Backstage passes to a TAFKAL80ETC concert")
-            if (@items[i].sell_in < 11)
-              if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
-              end
-            end
-            if (@items[i].sell_in < 6)
-              if (@items[i].quality < 50)
-                @items[i].quality = @items[i].quality + 1
-              end
-            end
+          if item.sell_in < 6
+            increase_item_quality(item)
           end
         end
       end
-      if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-        @items[i].sell_in = @items[i].sell_in - 1;
-      end
-      if (@items[i].sell_in < 0)
-        if (@items[i].name != "Aged Brie")
-          if (@items[i].name != "Backstage passes to a TAFKAL80ETC concert")
-            if (@items[i].quality > 0)
-              if (@items[i].name != "Sulfuras, Hand of Ragnaros")
-                @items[i].quality = @items[i].quality - 1
-              end
-            end
-          else
-            @items[i].quality = @items[i].quality - @items[i].quality
+    end
+    if item.name != "Sulfuras, Hand of Ragnaros"
+      item.sell_in -= 1
+    end
+    if item.sell_in < 0
+      if item.name != "Aged Brie"
+        if item.name != "Backstage passes to a TAFKAL80ETC concert"
+          if item.name != "Sulfuras, Hand of Ragnaros"
+            decrease_item_quality(item)
           end
         else
-          if (@items[i].quality < 50)
-            @items[i].quality = @items[i].quality + 1
-          end
+          item.quality = 0
         end
+      else
+        increase_item_quality(item)
       end
+    end
+  end
+
+  def decrease_item_quality(item)
+    if item.quality > 0
+      item.quality -= 1
+    end
+  end
+
+  def increase_item_quality(item)
+    if item.quality < MAX_QUALITY
+      item.quality += 1
     end
   end
 
--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -2,7 +2,7 @@ require "approvals/rspec"
 
 RSpec.describe GildedRose do
   let(:items) do
-    subject.instance_variable_get(:@items).map do |item|
+    subject.items.map do |item|
       "Name: #{item.name} Sell-in: #{item.sell_in} Quality: #{item.quality}"
     end
   end
@@ -11,6 +11,29 @@ RSpec.describe GildedRose do
     specify "after #{i} updates" do
       i.times { subject.update_quality }
       verify { items }
+    end
+  end
+
+  describe "conjured items" do
+    let(:item) { Item.new("Conjured something", 5, 10) }
+    before do
+      subject.update_item(item)
+    end
+
+    it "decreases quality by 2 each day" do
+      expect(item.quality).to eq 8
+    end
+
+    it "decreases sell_in by 1 each day" do
+      expect(item.sell_in).to eq 4
+    end
+
+    context "past the sell_in date" do
+      let(:item) { Item.new("Conjured old item", -1, 10) }
+
+      it "decreases twice as fast" do
+        expect(item.quality).to eq 6
+      end
     end
   end
 end
