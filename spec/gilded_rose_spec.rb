75cdc8e1a7b51e9011ba9d7a6eef785a67304c59
Randy Coulman <rcoulman@gmail.com>
2015-04-22 14:31:43 -0400

    Add characterization tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,8 +1,16 @@
-RSpec.describe GildedRose do
+require "approvals/rspec"
 
-  it "should do something" do
-    subject.update_quality
+RSpec.describe GildedRose do
+  let(:items) do
+    subject.instance_variable_get(:@items).map do |item|
+      "Name: #{item.name} Sell-in: #{item.sell_in} Quality: #{item.quality}"
+    end
   end
 
-
+  20.times do |i|
+    specify "after #{i} updates" do
+      i.times { subject.update_quality }
+      verify { items }
+    end
+  end
 end
