2ef124b6efe7ce01189f83470a1e17fea5c6d030
Randy Coulman <rcoulman@gmail.com>
2015-06-30 20:24:09 -0700

    Extract strings to constants

--- lib/gilded_rose/gilded_rose.rb
+++ lib/gilded_rose/gilded_rose.rb
@@ -4,29 +4,33 @@ class GildedRose
 
   @items = []
 
+  BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+  AGED_BRIE = "Aged Brie"
+  SULFURAS = "Sulfuras, Hand of Ragnaros"
+
   def initialize
     @items = []
     @items << Item.new("+5 Dexterity Vest", 10, 20)
-    @items << Item.new("Aged Brie", 2, 0)
+    @items << Item.new(AGED_BRIE, 2, 0)
     @items << Item.new("Elixir of the Mongoose", 5, 7)
-    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
-    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    @items << Item.new(SULFURAS, 0, 80)
+    @items << Item.new(BACKSTAGE_PASSES, 15, 20)
     @items << Item.new("Conjured Mana Cake", 3, 6)
   end
 
   def update_quality
 
     @items.each do |current_item|
-      if current_item.name != "Aged Brie" && current_item.name != "Backstage passes to a TAFKAL80ETC concert"
+      if current_item.name != AGED_BRIE && current_item.name != BACKSTAGE_PASSES
         if current_item.quality > 0
-          if current_item.name != "Sulfuras, Hand of Ragnaros"
+          if current_item.name != SULFURAS
             current_item.quality -= 1
           end
         end
       else
         if current_item.quality < 50
           current_item.quality += 1
-          if current_item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if current_item.name == BACKSTAGE_PASSES
             if current_item.sell_in < 11
               if current_item.quality < 50
                 current_item.quality += 1
@@ -40,14 +44,14 @@ class GildedRose
           end
         end
       end
-      if current_item.name != "Sulfuras, Hand of Ragnaros"
+      if current_item.name != SULFURAS
         current_item.sell_in -= 1
       end
       if current_item.sell_in < 0
-        if current_item.name != "Aged Brie"
-          if current_item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if current_item.name != AGED_BRIE
+          if current_item.name != BACKSTAGE_PASSES
             if current_item.quality > 0
-              if current_item.name != "Sulfuras, Hand of Ragnaros"
+              if current_item.name != SULFURAS
                 current_item.quality -= 1
               end
             end
