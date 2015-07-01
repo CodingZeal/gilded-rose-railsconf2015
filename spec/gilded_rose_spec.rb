dc26257459ae54533c62a8776bf19c854d95acaa
Randy Coulman <rcoulman@gmail.com>
2015-06-30 20:10:43 -0700

    Add approvals specs

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,8 +1,16 @@
-RSpec.describe GildedRose do
+require 'approvals/rspec'
 
-  it "should do something" do
-    subject.update_quality
+RSpec.describe GildedRose do
+  20.times do |i|
+    specify "after #{i} updates" do
+      i.times { subject.update_quality }
+      verify { items }
+    end
   end
 
-
+  def items
+    subject.instance_variable_get(:@items).map do |item|
+      "#{item.name}: quality: #{item.quality} sell_in: #{item.sell_in}"
+    end
+  end
 end
