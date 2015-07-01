626c33fb54640c5a7940237f3f3038c6b9cb7433
Randy Coulman <rcoulman@gmail.com>
2015-06-30 21:31:36 -0700

    Support conjured items properly

--- lib/gilded_rose/gilded_rose.rb
+++ lib/gilded_rose/gilded_rose.rb
@@ -8,6 +8,8 @@ class WrappedItem < SimpleDelegator
         AgedBrie.new(item)
       when /Backstage passes/
         BackstagePasses.new(item)
+      when /Conjured/
+        ConjuredItem.new(item)
       when /Sulfuras/
         LegendaryItem.new(item)
       else
@@ -25,7 +27,15 @@ class WrappedItem < SimpleDelegator
   end
 
   def quality_adjustment
-    expired? ? -2 : -1
+    expired? ? expired_adjustment : normal_adjustment
+  end
+
+  def expired_adjustment
+    2 * normal_adjustment
+  end
+
+  def normal_adjustment
+    -1
   end
 
   def make_older
@@ -44,22 +54,30 @@ class WrappedItem < SimpleDelegator
 end
 
 class AgedBrie < WrappedItem
-  def quality_adjustment
-    expired? ? 2 : 1
+  def normal_adjustment
+    1
   end
 end
 
 class BackstagePasses < WrappedItem
-  def quality_adjustment
-    if expired?
-      -quality
-    elsif sell_in < 5
+  def normal_adjustment
+    if sell_in < 5
       3
     elsif sell_in < 10
       2
     else
       1
     end
+  end
+
+  def expired_adjustment
+    -quality
+  end
+end
+
+class ConjuredItem < WrappedItem
+  def normal_adjustment
+    2 * super
   end
 end
 
@@ -70,17 +88,16 @@ class LegendaryItem < WrappedItem
 end
 
 class GildedRose
-
-  @items = []
+  attr_reader :items
 
   def initialize
     @items = []
-    @items << Item.new("+5 Dexterity Vest", 10, 20)
-    @items << Item.new("Aged Brie", 2, 0)
-    @items << Item.new("Elixir of the Mongoose", 5, 7)
-    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
-    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
-    @items << Item.new("Conjured Mana Cake", 3, 6)
+    items << Item.new("+5 Dexterity Vest", 10, 20)
+    items << Item.new("Aged Brie", 2, 0)
+    items << Item.new("Elixir of the Mongoose", 5, 7)
+    items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
+    items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    items << Item.new("Conjured Mana Cake", 3, 6)
   end
 
   def update_quality
