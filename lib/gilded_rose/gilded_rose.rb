b667285c2a5347c63395256678a0966a7133f079
Randy Coulman <rcoulman@gmail.com>
2015-06-30 20:34:19 -0700

    Extract update to WrappedItem

--- lib/gilded_rose/gilded_rose.rb
+++ lib/gilded_rose/gilded_rose.rb
@@ -2,34 +2,12 @@ require_relative 'item'
 require 'delegate'
 
 class WrappedItem < SimpleDelegator
-  def expired?
-    sell_in < 0
-  end
-end
-
-class GildedRose
-
-  @items = []
-
   BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
   AGED_BRIE = "Aged Brie"
   SULFURAS = "Sulfuras, Hand of Ragnaros"
 
-  def initialize
-    @items = []
-    @items << Item.new("+5 Dexterity Vest", 10, 20)
-    @items << Item.new(AGED_BRIE, 2, 0)
-    @items << Item.new("Elixir of the Mongoose", 5, 7)
-    @items << Item.new(SULFURAS, 0, 80)
-    @items << Item.new(BACKSTAGE_PASSES, 15, 20)
-    @items << Item.new("Conjured Mana Cake", 3, 6)
-  end
-
-  def update_quality
-    @items.each { |item| update_item(WrappedItem.new(item)) }
-  end
-
-  def update_item(current_item)
+  def update
+    current_item = self
     if current_item.name != AGED_BRIE && current_item.name != BACKSTAGE_PASSES
       if current_item.quality > 0
         if current_item.name != SULFURAS
@@ -75,4 +53,30 @@ class GildedRose
     end
   end
 
+  def expired?
+    sell_in < 0
+  end
+end
+
+class GildedRose
+
+  @items = []
+
+  def initialize
+    @items = []
+    @items << Item.new("+5 Dexterity Vest", 10, 20)
+    @items << Item.new("Aged Brie", 2, 0)
+    @items << Item.new("Elixir of the Mongoose", 5, 7)
+    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
+    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    @items << Item.new("Conjured Mana Cake", 3, 6)
+  end
+
+  def update_quality
+    @items.each { |item| update_item(WrappedItem.new(item)) }
+  end
+
+  def update_item(current_item)
+    current_item.update
+  end
 end
